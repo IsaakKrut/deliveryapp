f6b3eae445d2dd0481ef85b0e919f25222b619df
Isaak <krutisaak099@gmail.com>
2020-09-29 12:05:50 -0400

    Refactored to a multi-module Maven Project. Closes #4

---
+++ deliveryapp-data/src/main/java/com/isaakkrut/deliveryapp/data/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package com.isaakkrut.deliveryapp.data.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource datasource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/fooddelivery")
+                .username("root")
+                .password("1234")
+                .build();
+    }
+}
