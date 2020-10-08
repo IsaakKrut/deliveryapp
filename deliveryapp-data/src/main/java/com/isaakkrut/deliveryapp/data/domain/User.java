b63a92bbcfc94d8cfa9556aa6b1df14e11f6796f
Isaak <krutisaak099@gmail.com>
2020-10-08 17:59:23 -0400

    Implemented Builder pattern for class User

--- deliveryapp-data/src/main/java/com/isaakkrut/deliveryapp/data/domain/User.java
+++ deliveryapp-data/src/main/java/com/isaakkrut/deliveryapp/data/domain/User.java
@@ -1,10 +1,7 @@
 package com.isaakkrut.deliveryapp.data.domain;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -15,12 +12,33 @@ import java.time.LocalDate;
 @Table(name="users")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class User extends Base {
+
+    @Builder
+    public User(Long id, String email, String password, String firstName, String lastName, LocalDate birthDate) {
+        super(id);
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
 
     private String email;
     private String password;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
+
+    public void clear(){
+        this.setId(null);
+        this.email = null;
+        this.password = null;
+        this.firstName = null;
+        this.lastName = null;
+        this.birthDate = null;
+    }
 }
