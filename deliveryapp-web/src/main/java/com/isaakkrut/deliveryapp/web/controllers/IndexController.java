4a6d2781531661ccf2e92957bdcdf638a70f58f3
Isaak <krutisaak099@gmail.com>
2020-10-08 11:34:58 -0400

    Created mappings for menu display and adding new items to the card

--- deliveryapp-web/src/main/java/com/isaakkrut/deliveryapp/web/controllers/IndexController.java
+++ deliveryapp-web/src/main/java/com/isaakkrut/deliveryapp/web/controllers/IndexController.java
@@ -1,37 +1,67 @@
 package com.isaakkrut.deliveryapp.web.controllers;
 
-import com.isaakkrut.deliveryapp.data.services.jpa.CategoryJpaService;
+import com.isaakkrut.deliveryapp.data.domain.CategoryListDTO;
+import com.isaakkrut.deliveryapp.data.domain.Item;
+import com.isaakkrut.deliveryapp.data.domain.Order;
+import com.isaakkrut.deliveryapp.data.services.CategoryService;
+import com.isaakkrut.deliveryapp.data.services.ItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@SessionAttributes("order")
 public class IndexController {
-    private final CategoryJpaService categoryJpaService;
 
-    public IndexController(CategoryJpaService categoryJpaService) {
-        this.categoryJpaService = categoryJpaService;
+    private final CategoryService categoryService;
+    private final ItemService itemService;
+
+    public IndexController(CategoryService categoryService, ItemService itemService) {
+        this.categoryService = categoryService;
+        this.itemService = itemService;
+    }
+
+    @ModelAttribute("order")
+    public Order order(){
+        return new Order();
+
     }
 
 
     @RequestMapping({"", "/", "/home"})
     public String getIndexPage(Model model){
-        model.addAttribute("number", categoryJpaService.findAll().stream().count());
+        model.addAttribute("order", new Order());
         return "index";
     }
 
     @RequestMapping("/menu")
-    public String getMenu(){
-        return "notImplemented";
+    public String getMenu(Model model, @ModelAttribute("order")  Order order){
+        CategoryListDTO categories = new CategoryListDTO(itemService.findAll(), categoryService.findAll());
+        model.addAttribute("categoriesDTO", categories);
+        model.addAttribute("order", order);
+        return "menu";
     }
 
+    @PostMapping("/order/items/{id}")
+    public String addItemToTheCart(@PathVariable Long id, @SessionAttribute("order") Order order){
+
+        Item item = itemService.findById(id);
+        order.addItem(item);
+        //attributes.addFlashAttribute("order", order);
+        return "redirect:/menu";
+    }
+
+
     @RequestMapping("/checkout")
-    public String getCheckoutPage(){
+    public String getCheckoutPage(Model model){
         return "notImplemented";
     }
 
     @RequestMapping("/login")
-    public String getLoginPage(){
+    public String getLoginPage(Model model){
         return "notImplemented";
     }
 
